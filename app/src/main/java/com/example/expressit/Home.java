a1c7dcd566d23843ab487948f9d9e571824e2c26
Prottoy <prottoy.r.ahmed@gmail.com>
2020-12-25 06:14:18 -0500

    before tts

--- app/src/main/java/com/example/expressit/Home.java
+++ app/src/main/java/com/example/expressit/Home.java
@@ -2,15 +2,42 @@ package com.example.expressit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
+        btn1 = findViewById(R.id.button1);
+        btn2 = findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity();
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNewActivity2();
+            }
+        });
+    }
+    public void openNewActivity(){
+        Intent intent = new Intent(this,Text_to_speech.class);
+        startActivity(intent);
+    }
+
+    public void openNewActivity2(){
+        Intent intent = new Intent(this,Speecch_to_text.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/expressit/Speecch_to_text.java
@@ -0,0 +1,14 @@
+package com.example.expressit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Speecch_to_text extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speecch_to_text);
+    }
+}
