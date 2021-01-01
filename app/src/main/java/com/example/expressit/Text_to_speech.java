6d274468d6b6d4742a8cf5ec6068aaa446ee80da
Prottoy <prottoy.r.ahmed@gmail.com>
2021-01-01 14:11:28 -0500

    before tts

--- app/src/main/java/com/example/expressit/Text_to_speech.java
+++ app/src/main/java/com/example/expressit/Text_to_speech.java
@@ -3,12 +3,65 @@ package com.example.expressit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Text_to_speech extends AppCompatActivity {
+ private Button btnspeak;
+ private EditText editbox;
+ private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_text_to_speech2);
+        setContentView(R.layout.activity_text_to_speech);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        btnspeak = findViewById(R.id.button);
+        editbox = findViewById(R.id.entertext);
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.US);
+
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Toast.makeText(getApplicationContext(), "Language not supported", Toast.LENGTH_SHORT).show();
+                    } else {
+                        btnspeak.setEnabled(true);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Init failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+       btnspeak.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               speak() ;
+           }
+       });
+    }
+    private void speak() {
+        String text = editbox.getText().toString();
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 }
