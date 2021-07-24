d5efb1efb669005484098737c0ddcdce86fb7805
Maruf Pulok <maruf12@student.sust.edu>
2021-07-24 10:50:17 +0600

    Test Commit

--- app/src/main/java/com/example/expressit/Speecch_to_text.java
+++ app/src/main/java/com/example/expressit/Speecch_to_text.java
@@ -3,12 +3,18 @@ package com.example.expressit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Speecch_to_text extends AppCompatActivity {
+    private TextView txvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speecch_to_text);
+        //txvResult = (TextView) findViewById(R.id.txvResult);
+
     }
+
+
 }
