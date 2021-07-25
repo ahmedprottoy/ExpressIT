b3b2746e9ca0173b6ac926278e0c22b9a56c2d3e
Maruf Pulok <maruf12@student.sust.edu>
2021-07-25 22:43:38 +0600

    stt commit2

--- app/src/main/java/com/example/expressit/Speecch_to_text.java
+++ app/src/main/java/com/example/expressit/Speecch_to_text.java
@@ -1,52 +1,51 @@
 package com.example.expressit;
-
+import android.content.ActivityNotFoundException;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.speech.RecognizerIntent;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.view.View;
 
 import java.util.ArrayList;
-import java.util.Locale;
 
-public class Speecch_to_text extends AppCompatActivity {
-    private TextView txvRslt;
+public class Speecch_to_text extends AppCompatActivity{
+    private static final int REQUEST_CODE = 100;
+    private TextView textOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speecch_to_text);
+        textOutput= (TextView) findViewById(R.id.textOutput);
 
     }
 
-    public void getSpeechInput(View view) {
+
+    public void onClick(View v)
+    {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
+        try {
+            startActivityForResult(intent, REQUEST_CODE);
+        } catch (ActivityNotFoundException a) {
 
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, 10);
-        } else {
-            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
         }
     }
 
-    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
         switch (requestCode) {
-            case 10:
-                if (resultCode == RESULT_OK && data != null) {
+            case REQUEST_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txvRslt.setText(result.get(0));
+                    textOutput.setText(result.get(0));
                 }
                 break;
+            }
+
         }
     }
 }
