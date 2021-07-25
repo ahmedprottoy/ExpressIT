1fcfe826f2819aec720e0180502db09cb5b40819
Prottoy <prottoy.r.ahmed@gmail.com>
2021-07-25 21:21:29 +0600

    Navigation Menu created

--- app/src/main/java/com/example/expressit/Home.java
+++ app/src/main/java/com/example/expressit/Home.java
@@ -1,21 +1,42 @@
 package com.example.expressit;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+//import android.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
     Button btn1,btn2;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
         btn1 = findViewById(R.id.button1);
         btn2 = findViewById(R.id.button2);
         btn1.setOnClickListener(new View.OnClickListener() {
